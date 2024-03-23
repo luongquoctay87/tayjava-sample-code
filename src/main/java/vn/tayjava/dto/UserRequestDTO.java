165de34d36d887c383c4c886042d44784ca4866e
luongquoctay87 <luongquoctay87@gmail.com>
2024-03-23 19:10:57 +0700

    Bài 2 - Xây Dựng Ứng Dụng Hello World

---
+++ src/main/java/vn/tayjava/dto/UserRequestDTO.java
@@ -0,0 +1,49 @@
+package vn.tayjava.dto;
+
+import java.io.Serializable;
+
+public class UserRequestDTO implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public UserRequestDTO(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
