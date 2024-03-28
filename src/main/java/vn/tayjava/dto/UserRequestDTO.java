aeabf5453d3f182f24aa7f5b51492225f108a265
luongquoctay87 <luongquoctay87@gmail.com>
2024-03-29 04:22:37 +0700

    Validate input data: parameter, body, pathvariable

--- src/main/java/vn/tayjava/dto/UserRequestDTO.java
+++ src/main/java/vn/tayjava/dto/UserRequestDTO.java
@@ -1,12 +1,45 @@
 package vn.tayjava.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+import vn.tayjava.util.PhoneNumber;
+
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
 
 public class UserRequestDTO implements Serializable {
+
+    @NotBlank(message = "firstName must be not blank") // Khong cho phep gia tri blank
     private String firstName;
+
+    @NotNull(message = "lastName must be not null") // Khong cho phep gia tri null
     private String lastName;
+
+    @Email(message = "email invalid format") // Chi chap nhan nhung gia tri dung dinh dang email
     private String email;
+
+    //@Pattern(regexp = "^\\d{10}$", message = "phone invalid format")
+    @PhoneNumber(message = "phone invalid format")
     private String phone;
+
+    @NotNull(message = "dateOfBirth must be not null")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private Date dateOfBirth;
+
+    @NotNull(message = "username must be not null")
+    private String username;
+
+    @NotNull(message = "password must be not null")
+    private String password;
+
+    @NotEmpty(message = "addresses can not empty")
+    private Set<Address> addresses;
 
     public UserRequestDTO(String firstName, String lastName, String email, String phone) {
         this.firstName = firstName;
@@ -15,35 +48,142 @@ public class UserRequestDTO implements Serializable {
         this.phone = phone;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public static class Address {
+        private String apartmentNumber;
+        private String floor;
+        private String building;
+        private String streetNumber;
+        private String street;
+        private String city;
+        private String country;
+        private Integer addressType;
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+        public String getApartmentNumber() {
+            return apartmentNumber;
+        }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+        public void setApartmentNumber(String apartmentNumber) {
+            this.apartmentNumber = apartmentNumber;
+        }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+        public String getFloor() {
+            return floor;
+        }
+
+        public void setFloor(String floor) {
+            this.floor = floor;
+        }
+
+        public String getBuilding() {
+            return building;
+        }
+
+        public void setBuilding(String building) {
+            this.building = building;
+        }
+
+        public String getStreetNumber() {
+            return streetNumber;
+        }
+
+        public void setStreetNumber(String streetNumber) {
+            this.streetNumber = streetNumber;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public Integer getAddressType() {
+            return addressType;
+        }
+
+        public void setAddressType(Integer addressType) {
+            this.addressType = addressType;
+        }
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPhone() {
         return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
     }
 }
