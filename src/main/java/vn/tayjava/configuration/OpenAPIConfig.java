35268d398e332a525611331b17cde800d334bd32
TayLQ <luongquoctay87@gmail.com>
2024-04-13 17:21:39 +0700

    update mvn

--- src/main/java/vn/tayjava/configuration/OpenAPIConfig.java
+++ src/main/java/vn/tayjava/configuration/OpenAPIConfig.java
@@ -8,11 +8,13 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
 
 @Configuration
+@Profile("!prod")
 public class OpenAPIConfig {
 
     @Bean
