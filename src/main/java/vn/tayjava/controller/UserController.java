68b9a803194636976ac90bc72c9d9c84ce51ae56
TayLQ <luongquoctay87@gmail.com>
2024-04-30 11:09:51 +0700

    add trycatch

--- src/main/java/vn/tayjava/controller/UserController.java
+++ src/main/java/vn/tayjava/controller/UserController.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import vn.tayjava.dto.request.UserRequestDTO;
-import vn.tayjava.dto.response.ResponseData;
+import vn.tayjava.dto.response.ResponseFailure;
+import vn.tayjava.dto.response.ResponseSuccess;
 
 import java.util.List;
 
@@ -16,40 +17,65 @@ import java.util.List;
 public class UserController {
 
     @PostMapping("/")
-    public ResponseData<Integer> addUser(@Valid @RequestBody UserRequestDTO user) {
+    public ResponseSuccess addUser(@Valid @RequestBody UserRequestDTO user) {
         System.out.println("Request add user " + user.getFirstName());
-        return new ResponseData<>(HttpStatus.CREATED.value(), "User added successfully,", 1);
+
+        try {
+            return new ResponseSuccess(HttpStatus.CREATED, "User added successfully,", 1);
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @PutMapping("/{userId}")
-    public ResponseData<?> updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
+    public ResponseSuccess updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
         System.out.println("Request update userId=" + userId);
-        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User updated successfully");
+
+        try {
+            return new ResponseSuccess(HttpStatus.ACCEPTED, "User updated successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @PatchMapping("/{userId}")
-    public ResponseData<?> updateStatus(@Min(1) @PathVariable int userId, @RequestParam boolean status) {
+    public ResponseSuccess updateStatus(@Min(1) @PathVariable int userId, @RequestParam boolean status) {
         System.out.println("Request change status, userId=" + userId);
-        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User's status changed successfully");
+
+        try {
+            return new ResponseSuccess(HttpStatus.ACCEPTED, "User's status changed successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseData<?> deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
+    public ResponseSuccess deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
         System.out.println("Request delete userId=" + userId);
-        return new ResponseData<>(HttpStatus.NO_CONTENT.value(), "User deleted successfully");
+
+        try {
+            return new ResponseSuccess(HttpStatus.NO_CONTENT, "User deleted successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @GetMapping("/{userId}")
-    public ResponseData<UserRequestDTO> getUser(@PathVariable @Min(1) int userId) {
+    public ResponseSuccess getUser(@PathVariable @Min(1) int userId) {
         System.out.println("Request get user detail, userId=" + userId);
-        return new ResponseData<>(HttpStatus.OK.value(), "user", new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789"));
+
+        try {
+            return new ResponseSuccess(HttpStatus.OK, "user", new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789"));
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @GetMapping("/list")
-    public ResponseData<List<UserRequestDTO>> getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
+    public ResponseSuccess getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
                                                          @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
         System.out.println("Request get all of users");
-        return new ResponseData<>(HttpStatus.OK.value(), "users", List.of(new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789"),
+        return new ResponseSuccess(HttpStatus.OK, "users", List.of(new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789"),
                 new UserRequestDTO("Leo", "Messi", "leomessi@email.com", "0123456456")));
     }
 }
